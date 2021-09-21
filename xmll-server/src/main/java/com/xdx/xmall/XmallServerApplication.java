2ccea310d3b1182301e89175548c353debf69b04
xdx <2717814812@qq.com>
2021-09-21 09:23:18 -0700

    新建项目，pom，编写helloworld的controller

---
+++ xmll-server/src/main/java/com/xdx/xmall/XmallServerApplication.java
@@ -0,0 +1,13 @@
+package com.xdx.xmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmallServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XmallServerApplication.class, args);
+    }
+
+}
---
+++ xmll-server/src/test/java/com/xdx/xmall/XmallServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xdx.xmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmallServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
